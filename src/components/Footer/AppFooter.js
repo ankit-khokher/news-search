9c7ff96de1851e4820ad468483503b02677e992c
ankit khokher <ankit.khokher@ankits-MacBook-Pro.local>
2021-04-27 00:02:26 +0100

    feat: added haeader and footer components

---
+++ src/components/Footer/AppFooter.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="App-footer"></div>;
+};
+
+export default Footer;
---
+++ src/components/Header/AppHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AppHeader = () => {
+  return (
+    <div className="App-header">
+      <img
+        className="App-logo"
+        src="https://img01.bt.co.uk/s/assets/191120/images/logo/logo-2018.svg
+        "
+        alt="logo"
+        height="87"
+        width="100"
+      />
+    </div>
+  );
+};
+
+export default AppHeader;
