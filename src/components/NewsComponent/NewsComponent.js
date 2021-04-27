8879e414bf0b9cd822a7e78b8485555a965c7dfd
ankit khokher <ankit.khokher@ankits-MacBook-Pro.local>
2021-04-28 00:54:15 +0100

    feat: added onSearchClick event and called getNews to fetch news data

--- src/components/NewsComponent/NewsComponent.js
+++ src/components/NewsComponent/NewsComponent.js
@@ -1,12 +1,28 @@
 import SearchComponent from "../SearchComponent/SearchComponent";
-import "./NewsComponent.css";
 import NewsItemCard from "../NewsItemCard/NewsItemCard";
+import "./NewsComponent.css";
 
 const NewsComponent = () => {
+  const getNews = (searchQuery) => {
+    const url =
+      "https://newsapi.org/v2/everything?" +
+      "q=" +
+      encodeURI(searchQuery) +
+      "&from=2021-04-27&" +
+      "sortBy=popularity&" +
+      "apiKey=bac219e4c3f3440baf15916b96454ffe";
+    let result = fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+    return result.articles;
+  };
+
   return (
     <div className="News-container">
       <h1>BT React Code Test - by Ankit Khokher - 27/04/21</h1>
-      <SearchComponent />
+      <SearchComponent onSearchClick={getNews} />
       <NewsItemCard />
     </div>
   );
--- src/components/SearchComponent/SearchComponent.js
+++ src/components/SearchComponent/SearchComponent.js
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
 import "./SearchComponent.css";
 
 const SearchComponent = (props) => {
-  return <div className="Search-input"></div>;
+  const { onSearchClick } = props;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <div className="Search-input">
+      <input
+        value={searchQuery}
+        name="searchQuery"
+        type="text"
+        placeholder="Search for news"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          onSearchClick(searchQuery);
+          setSearchQuery("");
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default SearchComponent;
